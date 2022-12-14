a305cd552770af58e5848c7a5802c78d751daba3
Chef <Chef@DESKTOP-MLH0HHL>
2022-12-14 23:51:43 +0300

    Добавьте файлы проекта.

---
+++ Multinums/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+Numbers number = new();
+        Console.WriteLine("Введите число");
+        Console.WriteLine();
+        string enter = Console.ReadLine();
+        bool isParse = int.TryParse(enter, out var numbers);
+        if (isParse)
+        {
+            number.Func(numbers);
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Свободен");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Multinums/nums.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multinums
+{
+    internal class nums
+    { 
+  public void Func(int number)
+    {
+        int summ = 0;
+        for (int i = 1; i < number; i++)
+        {
+            if (i % 3 == 0)
+            {
+                summ += i;
+                Console.WriteLine();
+                Console.WriteLine($"{i} кратно трём");
+            }
+            else if (i % 5 == 0)
+            {
+                summ += i;
+                Console.WriteLine();
+                Console.WriteLine($"{i} кратно пяти");
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Сумма всех кратных чисел {summ}");
+    }
+}
+}
